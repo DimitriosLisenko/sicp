0fccc0826e007685d45299db32a58e72007ab7c2
dima <dimitrioslisenko@gmail.com>
2019-07-02 01:55:22 +0100

    Start writing some scheme

---
+++ chapter-1.1-exercises.scm
@@ -0,0 +1,14 @@
+;; exercise 1.3
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
+(define (min x y) (if (< x y) x y))
+(define (min-of-three x y z) (min (min x y) z))
+
+(define (the-exercise x y z) (cond ((= x (min-of-three x y z)) (sum-of-squares y z))
+				   ((= y (min-of-three x y z)) (sum-of-squares x z))
+				   ((= z (min-of-three x y z)) (sum-of-squares x y))))
+
+;; exercise 1.4
+;; that's pretty cool - so a compound expression can return an *operator* value that is the applied to other expressions
+;; in this particular case, it will add the absolute value of b to a
