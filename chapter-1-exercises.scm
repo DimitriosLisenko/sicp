3f2de87e2b8a2a5a762a92258cc35d74e3f95991
Dima <dima@cloud66.com>
2019-07-04 01:04:29 +0100

    Complete exercise 1.10

--- chapter-1-exercises.scm
+++ chapter-1-exercises.scm
@@ -84,4 +84,81 @@
 ;; OK fine... here it is:
 (define (improve guess x) (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
+;; exercise 1.9
+(define (+ a b)
+  (if (= a 0) b (inc (+ (dec a) b))))
+;; (+ 4 5)
+;; (inc (+ (dec 4) 5))
+;; (inc (+ 3 5))
+;; (inc (inc (+ (dec 3) 5)))
+;; (inc (inc (+ 2 5)))
+;; (inc (inc (inc (+ (dec 2) 5))))
+;; (inc (inc (inc (+ 1 5))))
+;; (inc (inc (inc (inc (+ (dec 1) 5)))))
+;; (inc (inc (inc (inc (+ 0 5)))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
+;; => this is a linear recursive process
 
+(define (+ a b)
+  (if (= a 0) b (+ (dec a) (inc b))))
+;; (+ 4 5)
+;; (+ (dec 4) (inc 5))
+;; (+ 3 6)
+;; (+ (dec 3) (inc 6))
+;; (+ 2 7)
+;; (+ (dec 2) (inc 7))
+;; (+ 1 8)
+;; (+ (dec 1) (inc 8))
+;; (+ 0 9)
+;; 9
+;; => this is a linear iterative process
+
+;; exercise 1.10
+;; (define (f n) (A 0 n))
+;; => (define (f n) (* 2 n))
+;; => computes 2 * n
+
+;; (define (g n) (A 1 n))
+;; => (define (g n) (A 0 (A 1 (n-1))))
+;; => (define (g n) (A 0 (A 0 (A 1 (n-2)))))
+;; => (define (g n) (A 0 (...(n-1 times) (A 1 (n - (n-1))))))
+;; => (define (g n) (A 0 (...(n-1 times) (A 1 1))))
+;; => (define (g n) (A 0 (...(n-1 times) 2)))
+;; => computes 2^n
+
+;; (define (h n) (A 2 n))
+;; => (define (h n) (A 1 (A 2 (n-1)))
+;; => (define (h n) (A 1 (A 1 (...(n-1 times) (A 2 (n - (n-1)))))))
+;; => (define (h n) (A 1 (A 1 (...(n-1 times) (A 2 1)))))
+;; => (define (h n) (A 1 (A 1 (...(n-2 times) (A 1 2)))))
+;; => because (A 1 n) computes 2^n, this computes 2^2^... where the power is raised (n-1) times
+
+;; (define (k n) (* 5 n n))
+;; => computer 5 * n^2
+
+;; (A 1 10) => 2^10 => 1024
+
+;; (A 2 4) => 2^2^2^2 => 65536
+
+;; (A 3 3)
+;; => (A 2 (A 3 2))
+;; => (A 2 (A 2 (A 3 1)))
+;; => (A 2 (A 2 2))
+;; => (A 2 4)
+;; => 65536 as above
+
+;; so...
+;; 2 + 2 + 2 ... + 2 (n times) gives 2 * n (A 0 n)
+;; 2 * 2 * 2 ... * 2 (n times) gives 2^n (A 1 n)
+;; 2 ^ 2 ^ 2 ... ^ 2 (n times) gives some mathematical function (A 2 n), let's give it symbol &, so 2 & n
+;; 2 & 2 & 2 ... & 2 (n times) gives some mathematical function (A 3 n)
+;; e.g. (a 3 3) => (2 & 2 & 2) => (2 & (2 ^ 2)) => (2 & 4) => (2 ^ 2 ^ 2 ^ 2) => 65536
+;; so the next iteration is 2 ^ 2 ^ 2 ... ^ 2 *65536 times*
+;; and so on - each increment of x in (A x y) gives a new operator of sorts
+;; pretty amazing stuff
+;; this can be notated using Knuth's up-arrow notation: https://en.wikipedia.org/wiki/Knuth%27s_up-arrow_notation
+ 
