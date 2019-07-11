8a3d9078bdb4231f9473564981f4252b875e8bdd
dima <dimitrioslisenko@gmail.com>
2019-07-11 01:20:50 +0100

    Implement exercise 1.32

---
+++ exercise-1.32.scm
@@ -0,0 +1,18 @@
+(define (accumulate-iterative combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(define (accumulate-recursive combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+         (accumulate-recursive combiner null-value term (next a) next b))))
+
+(define (identity x) x)
+(define (inc x) (+ x 1))
+
+(accumulate-recursive + 0 identity 1 inc 10) ;; sum from 1 to 10
+(accumulate-iterative * 1 identity 1 inc 10) ;; product from 1 to 10
