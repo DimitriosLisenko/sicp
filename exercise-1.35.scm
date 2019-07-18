04f03f237928a4166e2b998781fef7395a8b6a07
dima <dimitrioslisenko@gmail.com>
2019-07-19 00:45:02 +0100

    Implement exercise 1.35

---
+++ exercise-1.35.scm
@@ -0,0 +1,17 @@
+;; φ is a solution to φ^2 = φ + 1
+;; φ^2 = φ + 1
+;; => φ = (φ + 1) / φ 
+;; => φ = 1 + (1 / φ )
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+(fixed-point (lambda (x) (+ 1.0 (/ 1.0 x))) 2.0)
