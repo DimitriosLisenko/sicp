8c2065c663aa51e744f39c7c4d8c5d9f16cc908b
Dima <dima@cloud66.com>
2019-07-19 21:11:01 +0100

    Implement exercise 1.39

---
+++ exercise-1.39.scm
@@ -0,0 +1,16 @@
+(define (stairway-division-iterative numerator-function denominator-function steps)
+  (define (stairway-division-internal step result)
+    (if (= step 0)
+        result
+        (stairway-division-internal
+         (- step 1)
+         (/ (numerator-function step) (+ (denominator-function step) result)))))
+  (stairway-division-internal steps 0))
+
+(define (tan-sd x k)
+  (stairway-division-iterative
+   (lambda (i) (if (= i 1) x (* (* x x) -1)))
+   (lambda (i) (- (* i 2) 1))
+   k))
+;; 0.785398163397448309615660845819875721049292349843776455243 is pi/4
+(tan-sd 0.785398163397448309615660845819875721049292349843776455243 30)
