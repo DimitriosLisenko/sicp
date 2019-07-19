3bcdc6fbe887ab87a8edda1dcddb001cc47368da
Dima <dima@cloud66.com>
2019-07-19 11:11:18 +0100

    Implement the iterative version of the stairway division algorithm

--- exercise-1.37.scm
+++ exercise-1.37.scm
@@ -7,3 +7,13 @@
          (+ (denominator-function step) (stairway-division-internal (+ 1 step))))))
   (stairway-division-internal 1))
 (stairway-division-recursive (lambda (x) 1.0) (lambda (x) 1.0) 20)
+
+(define (stairway-division-iterative numerator-function denominator-function steps)
+  (define (stairway-division-internal step result)
+    (if (= step 0)
+        result
+        (stairway-division-internal
+         (- step 1)
+         (/ (numerator-function step) (+ (denominator-function step) result)))))
+  (stairway-division-internal steps 0))
+(stairway-division-iterative (lambda (x) 1.0) (lambda (x) 1.0) 20)
