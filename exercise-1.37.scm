c85482b55105b5f90e7230fad240d39c7c0ceb34
dima <dimitrioslisenko@gmail.com>
2019-07-19 02:19:56 +0100

    Partially implement exercise 1.37

---
+++ exercise-1.37.scm
@@ -0,0 +1,9 @@
+(define (stairway-division-recursive numerator-function denominator-function steps)
+  (define (stairway-division-internal step)
+    (if (= step steps)
+        0
+        (/
+         (numerator-function step)
+         (+ (denominator-function step) (stairway-division-internal (+ 1 step))))))
+  (stairway-division-internal 1))
+(stairway-division-recursive (lambda (x) 1.0) (lambda (x) 1.0) 20)
