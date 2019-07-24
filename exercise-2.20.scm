05cac985627c3d4cb8cdf42be6e9b3c0445e912d
Dima <dima@cloud66.com>
2019-07-24 11:28:10 +0100

    Implement two variants of same-parity - one that follows the same logic as append, and another that follows the same logic as reverse

--- exercise-2.20.scm
+++ exercise-2.20.scm
@@ -1,3 +1,4 @@
+;; this version of same-parity follows the same logic as reverse with an additional condition
 (define (same-parity . x)
   (define (correct-parity? y) (= (modulo (car x) 2) (modulo y 2)))
   (define (same-parity-internal res curr)
@@ -10,6 +11,21 @@
           res)
          (cdr curr))))
   (same-parity-internal (list) x))
+
+(same-parity)
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
+
+;; this version of same-parity follows the same logic as append with an additional condition
+(define (same-parity . x)
+  (define (correct-parity? y) (= (modulo (car x) 2) (modulo y 2)))
+  (define (same-parity-internal curr)
+    (if (null? curr)
+        (list)
+        (if (correct-parity? (car curr))
+            (cons (car curr) (same-parity-internal (cdr curr)))
+            (same-parity-internal (cdr curr)))))
+  (same-parity-internal x))
 
 (same-parity)
 (same-parity 1 2 3 4 5 6 7)
