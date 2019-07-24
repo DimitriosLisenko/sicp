819d18fc93366b93258b3a25aad6364082f52698
Dima <dima@cloud66.com>
2019-07-24 23:26:38 +0100

    Implement exercise 2.27

--- exercise-2.27.scm
+++ exercise-2.27.scm
@@ -10,5 +10,5 @@
 
 ;; my fucking god - it worked!
 (define x (list (list 1 2) (list 3 4) 5 (list 6 7 8)))
-(reverse x)
-(deep-reverse x)
+(reverse x) ;; ((6 7 8) 5 (3 4) (1 2))
+(deep-reverse x) ;; ((8 7 6) 5 (4 3) (2 1))
