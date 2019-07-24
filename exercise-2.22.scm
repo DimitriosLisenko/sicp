a7031d8932410a6d02710a7a056388a577a81820
Dima <dima@cloud66.com>
2019-07-24 21:58:17 +0100

    Implement exercise 2.22

---
+++ exercise-2.22.scm
@@ -0,0 +1,6 @@
+;; the first version produces the reverse because he runs cons on the first
+;; element of the list against answer, so it becomes the last element
+
+;; the second one will not create a list, it will return a list from the RHS
+
+;; funnily enough, I did both of these mistakes when doing exercise 2.18!
