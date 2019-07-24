68fd1a606ec1325e5d13b4a0fb98630f5db4391a
Dima <dima@cloud66.com>
2019-07-24 23:31:55 +0100

    Implement exercise 2.27

--- exercise-2.27.scm
+++ exercise-2.27.scm
@@ -12,3 +12,10 @@
 (define x (list (list 1 2) (list 3 4) 5 (list 6 7 8)))
 (reverse x) ;; ((6 7 8) 5 (3 4) (1 2))
 (deep-reverse x) ;; ((8 7 6) 5 (4 3) (2 1))
+
+(define y (list (list 1 2) (list 3 4) 5 (list 6 7 8 (list 9 10 (list 11 12) (list 13 14)))))
+(reverse y) ;; ((6 7 8 (9 10 (11 12) (13 14))) 5 (3 4) (1 2))
+(deep-reverse y) ;; ((((14 13) (12 11) 10 9) 8 7 6) 5 (4 3) (2 1))
+
+(deep-reverse (list))
+(deep-reverse (list (list)))
