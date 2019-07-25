031d90916891e99389f904b9590b8eb26fdfbfbd
dima <dimitrioslisenko@gmail.com>
2019-07-25 20:10:41 +0100

    Implement exercise 2.29

--- exercise-2.29.scm
+++ exercise-2.29.scm
@@ -25,3 +25,21 @@
   (+
    (branch-weight (left-branch mobile))
    (branch-weight (right-branch mobile))))
+
+(define (torque branch)
+  (*
+   (branch-weight branch)
+   (branch-length branch)))
+
+;; this is horribly inefficient at the moment because it calls torque -> branch-weight -> total-weight at each node
+(define (balanced? mobile)
+  (if (not (pair? mobile))
+      true
+      (and
+       (balanced? (branch-structure (left-branch mobile)))
+       (balanced? (branch-structure (right-branch mobile)))
+       (= (torque (left-branch mobile)) (torque (right-branch mobile))))))
+
+;; (define x (make-mobile (make-branch 1 1) (make-branch 1 1)))
+;; (define y (make-mobile (make-branch 2 5) (make-branch 5 x)))
+;; (balanced? y)
