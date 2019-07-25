60233d055a926db72e37532e835d6916ad1e2277
dima <dimitrioslisenko@gmail.com>
2019-07-26 00:31:19 +0100

    Implement exercise 2.30

---
+++ exercise-2.30.scm
@@ -0,0 +1,16 @@
+(define x (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(define (square-tree x)
+  (cond ((null? x) x)
+        ((not (pair? x)) (square x))
+        (else (cons (square-tree (car x)) (square-tree (cdr x))))))
+(square-tree x)
+
+(define (square-tree x)
+  (map
+   (lambda (x)
+     (if (not (pair? x))
+         (square x)
+         (square-tree x)))
+   x))
+(square-tree x)
