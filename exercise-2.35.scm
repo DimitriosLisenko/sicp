8b97c22897c787785f0d67b26bb2c748f5fec53f
dima <dimitrioslisenko@gmail.com>
2019-07-27 20:31:45 +0100

    Implement exercise 2.35

---
+++ exercise-2.35.scm
@@ -0,0 +1,21 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (count-leaves tree)
+  (accumulate
+   +
+   0
+   (map
+    (lambda (subtree)
+      (cond ((null? subtree) 0)
+            ((pair? subtree) (count-leaves subtree))
+            (else 1)))
+    tree)))
+
+(define x (cons (list 1 2) (list 3 4)))
+(count-leaves x)
+(count-leaves (list x x))
+(count-leaves (list (list)))
