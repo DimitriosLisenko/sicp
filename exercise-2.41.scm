eebe70c3fc5a54b840d3de67b300ef0a500adeba
dima <dimitrioslisenko@gmail.com>
2019-07-28 12:26:53 +0100

    Implement exercise 2.41

---
+++ exercise-2.41.scm
@@ -0,0 +1,33 @@
+(define (flatmap proc seq)
+  (fold-right append '() (map proc seq)))
+
+(define (enumerate-interval lower upper)
+  (if (> lower upper)
+      '()
+      (cons lower (enumerate-interval (+ 1 lower) upper))))
+
+(define (ordered-triplets n)
+  (flatmap
+   (lambda (i)
+     (flatmap
+      (lambda (j)
+        (map
+         (lambda (k) (list i j k))
+         (enumerate-interval 1 (- j 1))))
+      (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 n)))
+
+;; now that I think about it, this can also be written recursively
+;; give me all the ordered triplets for (- n 1) and for reach one, generate the additional results that are obtained by adding n
+;; the combination step is a bit weird though because e.g.
+;; all (ordered-triplets 3) returns ((3 2 1))
+;; so to get (ordered-triplets 4)
+;; you substitute 4 in all possible locations
+;; => ((3 2 1) (4 2 1) (3 4 1) (3 2 4))
+;; that's kinda dodgy, so I won't implement it
+
+(define (ordered-triplets-with-sum n s)
+  (filter
+   (lambda (triplet)
+     (= s (fold-right + 0 triplet)))
+   (ordered-triplets n)))
