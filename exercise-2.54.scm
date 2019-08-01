60e5fc541dd5cabd31f3ab2b29ef2a6733207bb7
dima <dimitrioslisenko@gmail.com>
2019-08-01 20:51:05 +0100

    Also check for numbers in eq?

--- exercise-2.54.scm
+++ exercise-2.54.scm
@@ -1,11 +1,20 @@
 (define (equal? lst mst)
   (cond
-   ((and (null? lst)
-         (null? mst)) true)
-   ((and (symbol? lst)
-         (symbol? mst))
+   ((and
+     (null? lst)
+     (null? mst))
+    true)
+   ((and
+     (symbol? lst)
+     (symbol? mst))
     (eq? lst mst))
-   ((and (pair? lst) (pair? mst))
+   ((and
+     (number? lst)
+     (number? mst))
+    (= lst mst))
+   ((and
+     (pair? lst)
+     (pair? mst))
     (and
      (equal? (car lst) (car mst))
      (equal? (cdr lst) (cdr mst))))
@@ -15,3 +24,5 @@
 (equal? '(this is a list) '(this is a list))
 (equal? '(this is a list) '(this (is a) list))
 (equal? '(this (is a) list) '(this (is a) list))
+(equal? '(1 2 3 (4 5) 6) '(1 2 3 (4 5) 6)) 
+(equal? '(1 2 3 (4 5) 6) '(1 2 3 (4 5 7) 6))
