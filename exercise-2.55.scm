73f2f1821083e7ff69db182a1d3c7b44bd8a62e0
Dima <dima@cloud66.com>
2020-01-03 00:14:40 +0000

    Clarify exercise 2.55

--- exercise-2.55.scm
+++ exercise-2.55.scm
@@ -1,5 +1,7 @@
 ;; (car ''abracadabra)
 ;; => (car (quote (quote abracadabra)))
-;; => (car (quote (quote abracadabra)))
+;; EDIT 2020-01-02: the way it apparently works in the interpreter is that the FIRST occurrence of quote will quote the remaining object
+;; therefore the OUTER quote quotes the INNER (quote abracadabra) list, such that it becomes a list of two *symbols*: quote and abracadabra
+;; i.e. the inner quote is NOT quote the procedure, but is quote a symbol
 ;; => (car (quote abracadabra))
 ;; => quote
