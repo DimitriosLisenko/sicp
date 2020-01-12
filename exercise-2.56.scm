3b16813ee3aa0a4f3c7ce0a53cda40fd838cb05d
dima <dimitrioslisenko@gmail.com>
2020-01-12 22:57:35 +0000

    Fully implement exercise 2.56

--- exercise-2.56.scm
+++ exercise-2.56.scm
@@ -1,35 +1,33 @@
-(define (=number? exp num) (and (number? exp) (= exp num)))
-
 (define (variable? x) (symbol? x))
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
-(define (sum? x) (and (pair? x) (eq? (car x) '+)))
-(define (addend s) (cadr s))
-(define (augend s) (caddr s))
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2))
          (+ a1 a2))
         (else (list '+ a1 a2))))
-
-(define (product? x) (and (pair? x) (eq? (car x) '*)))
-(define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
-
-(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e)) (expt b e))
+        (else (list '** b e))))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+(define (exponentiation? x) (and (pair? x) (eq? (car x) '**))) ;; can only handle numerical exponents, so maybe verify that it is
 (define (base e) (cadr e))
 (define (exponent e) (caddr e))
-(define (make-exponentiation b e)
-  (list '** b e))
-
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
@@ -46,10 +44,10 @@
           (exponent exp)
           (make-product
            (make-exponentiation
-            (base exp)
-            (- (exponent exp) 1))
+	     (base exp)
+	     (- (exponent exp) 1))
            (deriv (base exp) var))))
         (else
          (error "unknown expression type: DERIV" exp))))
 
-;; (deriv '(* 3 (** x 2)) 'x)
+;; (deriv '(** (* x y) 4) 'x)
