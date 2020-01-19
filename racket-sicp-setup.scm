6cf613654a76b44a4f8e9bea2ed0485b4a44e4eb
dima <dimitrioslisenko@gmail.com>
2020-01-19 13:56:52 +0000

    Added a setup file to make the printer output similarly to mit-scheme

---
+++ racket-sicp-setup.scm
@@ -0,0 +1,8 @@
+#lang sicp
+(#%require (only racket/base
+                 print-as-expression
+                 print-pair-curly-braces
+                 print-mpair-curly-braces))
+(print-as-expression      #f)
+(print-pair-curly-braces  #t)
+(print-mpair-curly-braces #f)
