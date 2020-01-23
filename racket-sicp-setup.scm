45ca11fd4c17659f1aba6f5e95820892a993e9eb
dima <dimitrioslisenko@gmail.com>
2020-01-23 23:56:47 +0000

    Remove all traces of the printer setup

--- racket-sicp-setup.scm
+++
@@ -1,8 +0,0 @@
-#lang sicp
-(#%require (only racket/base
-                 print-as-expression
-                 print-pair-curly-braces
-                 print-mpair-curly-braces))
-(print-as-expression      #f)
-(print-pair-curly-braces  #t)
-(print-mpair-curly-braces #f)
